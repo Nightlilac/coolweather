ed8a09ed568a6b6eebd0a242509a40568759090f
Nightlilac <Nightlilac@XFengdeMacBook-Pro.local>
2016-04-01 09:13:57 +0800

    First commit

---
+++ app/src/main/java/com/example/nightlilac/coolweather/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.nightlilac.coolweather.app;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.nightlilac.coolweather.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
