9eeca4a8eea7a30bba936b50e26725a69584834b
Nightlilac <Nightlilac@XFengdeMacBook-Pro.local>
2016-04-06 08:55:17 +0800

    第二次更新

---
+++ app/src/main/java/com/coolweather/app/util/HttpCallbackListener.java
@@ -0,0 +1,11 @@
+package com.coolweather.app.util;
+
+/**
+ * Created by Nightlilac on 16/4/1.
+ */
+public class HttpCallbackListener {
+
+    void onFinish(String response){}
+
+    void onError(Exception e){}
+}
